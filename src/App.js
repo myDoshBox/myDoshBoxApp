b871289b1eb64f67abfdcfd1eea0923e6f0dade5
Korex23 <ololokes@gmail.com>
2022-09-27 15:34:29 +0100

    added guest navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {
-  CompletedNotification,
-  CanceledNotification,
-  Refunded,
-  PendingConfirmation,
-  InDispute,
-  AwaitingDelivery,
-  PendingPayment,
-  Resolved,
-  Unresolved,
-} from "./components/NotificationComponent/NotificationComponents";
 
 function App() {
-  return (
-    <>
-      <CompletedNotification />
-      <CanceledNotification />
-      <Refunded />
-      <InDispute />
-      <PendingConfirmation />
-      <AwaitingDelivery />
-      <PendingPayment />
-      <Unresolved />
-      <Resolved />
-    </>
-  );
+  return;
 }
 
 export default App;
