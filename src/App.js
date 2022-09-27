0409aa88ea8e73031f253749f7af93b1c915d351
Korex23 <ololokes@gmail.com>
2022-09-27 07:41:40 +0100

    completed notifications components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  CompletedNotification,
+  CanceledNotification,
+  Refunded,
+  PendingConfirmation,
+  InDispute,
+  AwaitingDelivery,
+  PendingPayment,
+  Resolved,
+  Unresolved,
+} from "./components/NotificationComponent/NotificationComponents";
 
 function App() {
-  return;
+  return (
+    <>
+      <CompletedNotification />
+      <CanceledNotification />
+      <Refunded />
+      <InDispute />
+      <PendingConfirmation />
+      <AwaitingDelivery />
+      <PendingPayment />
+      <Unresolved />
+      <Resolved />
+    </>
+  );
 }
 
 export default App;
