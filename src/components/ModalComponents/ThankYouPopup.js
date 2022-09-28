881b62153fe866a70fe81000c6aa354c4bc74882
olasunkanmi <olasunkanmiidris15@gmail.com>
2022-09-28 15:50:56 +0100

    Componen build: Modal Component (in progress)

--- src/components/ModalComponents/ThankYouPopup.js
+++ src/components/ModalComponents/ThankYouPopup.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import ThankYouPopUpImg from '../../assets/images/Happy People.jpg'
 
 
-/**
- * Please Note that the ThankYouPopUp Modal works with the confirm button 
- * 
- **/ 
+/** Please Note that the ThankYouPopUp Modal works with the confirm button **/ 
 export const ThankYouPopUp = () => {
   const [show, setShow] = useState(false);
 
@@ -15,24 +13,24 @@ export const ThankYouPopUp = () => {
 
   return (
     <>
-      <Button className="border border-0 ThankYouPopUp-LaunchBtn" variant="primary" onClick={handleShow}>
+      <Button className="border border-0 Modal-Btn" variant="primary" onClick={handleShow}>
         Confirm
       </Button>
 
+        {/* <div className="ModalPopUp"> */}
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+        <Modal.Header className='text-center border border-0 justify-content-center flex-column pb-0'>
+          <img className='w-50' src={ThankYouPopUpImg} alt="...." />
+          <Modal.Title className='ModalTitle'>Thank You</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
+        <Modal.Body className='text-center border border-0 ModalMessage'>The entire team at MyDoshBox is very grateful to you for trusting us to keep your transactions secure. For any improvements you may have in mind please contact our customer care.</Modal.Body>
+        <Modal.Footer className='border border-0 text-center justify-content-center'>
+          <Button className="border border-0 Modal-Btn" variant="primary" onClick={handleClose}>
+            Home
+          </Button >
         </Modal.Footer>
       </Modal>
+        {/* </div> */}
     </>
   );
 }
