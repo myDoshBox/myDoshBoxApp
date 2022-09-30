4052cf6c78d3024bb4d2a238ffebf3bfaa8a5a5b
Korex23 <ololokes@gmail.com>
2022-09-30 18:27:12 +0100

    np commit

--- src/components/FormComponents.js/TransactionForms.js
+++ src/components/FormComponents.js/TransactionForms.js
@@ -2,6 +2,18 @@ import { useState } from "react";
 import { Card, Form } from "react-bootstrap";
 
 //EscrowInitiationForm
+export const EscrowInitiationForm = () => {
+  return (
+    <Card>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+      </Form>
+    </Card>
+  );
+};
 //SellerDeliverGoodsForm
 //BankTransferForm
 //BuyerConfirmProductDeliveryForm
