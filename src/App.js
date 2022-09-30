3ed0552f2b236dbe1b4ae2c38c39c3ee76e107e1
Korex23 <ololokes@gmail.com>
2022-09-30 10:13:06 +0100

    completed edit buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import {
+  EditProfileButton,
+  EditTicketButton,
+  ResendProductButton,
+  ContestComplaintButton,
+} from "./components/ButtonsComponent/EditButtons";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <EditProfileButton />
+      <EditTicketButton />
+      <ResendProductButton />
+      <ContestComplaintButton />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ButtonsComponent/EditButtons.js
+++ src/components/ButtonsComponent/EditButtons.js
@@ -1,3 +1,33 @@
+import { Button } from "react-bootstrap";
+
 //Edit Profile Button
+export const EditProfileButton = () => {
+  return (
+    <Button className="edit-profile-btn border-0 rounded-1">
+      Edit Profile
+    </Button>
+  );
+};
 //TicketButton
+export const EditTicketButton = () => {
+  return (
+    <Button className="edit-ticket-btn border-0 rounded-1">EDIT TICKET</Button>
+  );
+};
 //Resend Product
+export const ResendProductButton = () => {
+  return (
+    <Button className="edit-ticket-btn border-0 rounded-1">
+      RESEND PRODUCT
+    </Button>
+  );
+};
+
+//ContestComplaint Button
+export const ContestComplaintButton = () => {
+  return (
+    <Button className="edit-ticket-btn border-0 rounded-1">
+      CONTEST COMPLAINT
+    </Button>
+  );
+};
