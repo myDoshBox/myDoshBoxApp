1d4fa9cbb91ba8cd2ebcd081117c8d0c0e1284f0
Korex23 <ololokes@gmail.com>
2022-09-30 10:13:24 +0100

    completed edit buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-import {
-  EditProfileButton,
-  EditTicketButton,
-  ResendProductButton,
-  ContestComplaintButton,
-} from "./components/ButtonsComponent/EditButtons";
-
 function App() {
-  return (
-    <div>
-      <EditProfileButton />
-      <EditTicketButton />
-      <ResendProductButton />
-      <ContestComplaintButton />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
