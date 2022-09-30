3852bd2f3430c179e8e84c2c760fbbfc22aa5eb6
Korex23 <ololokes@gmail.com>
2022-09-30 11:46:01 +0100

    transaction buttons completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
-import { GuestNavbar } from "./components/NavbarComponents/TopNavbars";
-import {
-  CreateTransactionButton,
-  NewTransactionButton,
-  ProceedButton,
-  ConfirmDeliveryButton,
-  IAgreeButton,
-  ConfirmButton,
-  DeliverProductButton,
-} from "./components/ButtonsComponent/TransactionButtons";
 function App() {
-  return (
-    <div>
-      <GuestNavbar />
-      <CreateTransactionButton />
-      <NewTransactionButton />
-      <ProceedButton />
-      <ConfirmDeliveryButton />
-      <IAgreeButton />
-      <ConfirmButton />
-      <DeliverProductButton />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
