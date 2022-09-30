50b520275e2c6ca69a1351cc36cbdd6965507528
Korex23 <ololokes@gmail.com>
2022-09-30 07:15:11 +0100

    emphasized item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import { DeliveryConfirmedPopUp } from "./components/ModalComponents/DeliveryConfirmedPopUp";
-import { ThankYouPopup } from "./components/ModalComponents/ThankYouPopup";
+import { ThankYouPopUp } from "./components/ModalComponents/ThankYouPopup";
 import {
   BuyerTransactionStartedPopUp,
   SellerTransactionStartedPopUp,
 } from "./components/ModalComponents/TransactionStartedPopup";
 import { EscrowInitiationPopUp } from "./components/ModalComponents/EscrowInitiationPopUp";
+import {
+  GuestNavbar,
+  UserDashboardNavbar,
+} from "./components/NavbarComponents/TopNavbars";
+import {
+  CompletedNotification,
+  Refunded,
+  Resolved,
+  AwaitingDelivery,
+  Unresolved,
+  PendingConfirmation,
+  PendingPayment,
+  InDispute,
+} from "./components/NotificationComponent/NotificationComponents";
+
+import {
+  DashboardIcon,
+  SettingsIcon,
+  TicketsIcon,
+  AnalyticsIcon,
+  NotificationIcon,
+  UsersIcon,
+  ChatIcon,
+  NavTransactionIcon,
+  LogoutIcon,
+  DisputeIcon,
+} from "./components/IconComponent/SideNavIcons";
 
 function App() {
   return (
@@ -13,6 +40,27 @@ function App() {
       <EscrowInitiationPopUp />
       <SellerTransactionStartedPopUp />
       <DeliveryConfirmedPopUp />
+      <ThankYouPopUp />
+      <GuestNavbar />
+      <UserDashboardNavbar />
+      <CompletedNotification />
+      <AwaitingDelivery />
+      <InDispute />
+      <PendingConfirmation />
+      <PendingPayment />
+      <Unresolved />
+      <Refunded />
+      <Resolved />
+      <AnalyticsIcon />
+      <DashboardIcon />
+      <SettingsIcon />
+      <TicketsIcon />
+      <NotificationIcon />
+      <UsersIcon />
+      <ChatIcon />
+      <NavTransactionIcon />
+      <LogoutIcon />
+      <DisputeIcon />
     </div>
   );
 }
