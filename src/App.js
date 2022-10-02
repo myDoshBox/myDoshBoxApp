4fe10bcc0123dc32074c50fd0d46642b33e47496
olasunkanmi <olasunkanmiidris15@gmail.com>
2022-10-02 17:13:08 +0100

    Component Build: UpdateForms completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import {UpdateProfile,UpdatePassword, UpdateBankDetails, UserUpdateProfile } from "./components/FormComponents.js/UpdateForms"
+
 function App() {
   return (
     <div>
-      {/* <UpdateProfile/> */}
-      {/* <UpdatePassword/> */}
-      {/* <UpdateBankDetails /> */}
-      <UserUpdateProfile/>
+      
     </div>
   );
 }
