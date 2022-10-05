2f4d229332eb33233b37fe1c005e6417e049944d
Christian Gabriel <goldenlampstandministry777@gmail.com>
2022-10-05 03:10:21 +0100

    Centered all the text in the Pricing table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Tablecomponents from "./components/TableComponents/PricingTable";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Tablecomponents />
+    </>
+  );
 }
 
 export default App;
