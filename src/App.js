1230ba7ea92f38f784bda4ee23d600414969bf9f
Oluwasheyi <martinsakin7@gmail.com>
2022-10-04 19:00:02 -0700

    Merging

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import Tablecomponents from "./components/TableComponents/PricingTable";
 // import FooterComponents from "./components/FooterComponents";
-// import { AdminSidenav } from "./components/NavbarComponents/SideNavbar";
+import { AdminSidenav } from "./components/NavbarComponents/SideNavbar";
 
 function App() {
   return (
     <div>
       {/* <Tablecomponents /> */}
       {/* <FooterComponents /> */}
-      {/* <AdminSidenav /> */}
+      <AdminSidenav />
     </div>
   );
 }
