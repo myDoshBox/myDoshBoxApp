95c56f46a117386e2b88d4527c23ac29bdd96116
Korex23 <ololokes@gmail.com>
2022-10-13 20:25:42 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-import {
-  OpenConflictsCard,
-  OngoingConflictCard,
-  ClosedConflictsCard,
-  AllConflictsCard,
-  ReportDefectCard,
-  InitiateDisputeCard,
-  MiniDisputesCard,
-} from "./components/CardComponents/ConflictIssuesCards";
-
-import { IndividualTransactionsCard } from "./components/CardComponents/TransactionCards";
-
 function App() {
-  return (
-    <div>
-      {/* <OpenConflictsCard />
-      <OngoingConflictCard />
-      <ClosedConflictsCard />
-      <AllConflictsCard />
-      <ReportDefectCard />
-      <InitiateDisputeCard />
-      <MiniDisputesCard /> */}
-      <IndividualTransactionsCard />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
