74e58ca7fbde6c6b7744b77f9c307a574b4e6f39
olasunkanmi <olasunkanmiidris15@gmail.com>
2022-10-13 15:23:00 +0100

    back to merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { TotalUsersCard } from "./components/CardComponents/UsersCards";
+import { NotificationCard } from "./components/CardComponents/InfoCards";
+
 
 function App() {
   return (
     <div>
       <TotalUsersCard/>
+      <NotificationCard/>
     </div>
   );
 }
