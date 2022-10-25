bd76087f6e853679513bc81220ebcaedefb849e8
Korex23 <ololokes@gmail.com>
2022-10-25 15:27:10 +0100

    corrected initiate transactions card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { FAQsCard } from "./components/CardComponents/ContactAndInfoCards";
+import {
+  BuyerTransactCard,
+  InitiateTransactionsCard,
+  SellerTransactCard,
+} from "./components/CardComponents/TransactionCards";
 // import { GuestNavbar } from "./components/NavbarComponents/TopNavbars";
 
 function App() {
   return (
     <div>
       <FAQsCard />
+      <BuyerTransactCard />
+      <SellerTransactCard />
+      <InitiateTransactionsCard />
     </div>
   );
 }
