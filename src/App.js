66742331d119cd8eea3d129f3e6cf0e2378c5d87
Christian Gabriel <goldenlampstandministry777@gmail.com>
2022-12-13 00:38:27 +0100

    Done Cleaning Up the AboutUsCard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {
-	ConflictIssuesCards,
-	OngoingConflictCard,
-	OpenConflictsCard,
-} from "./components/CardComponents/ConflictIssuesCards";
-import { ContactUsCard } from "./components/CardComponents/ContactAndInfoCards";
-import About from "./pages/About";
 
 function App() {
-	return (
-		<>
-			<About />
-		</>
-	);
+  return <></>;
 }
 
 export default App;
