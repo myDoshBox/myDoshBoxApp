017addff38c2d85b67e7fb23df7468ec0e9f1568
profgreatwonder <muoghaluchidinma@gmail.com>
2022-12-23 08:21:06 +0100

    added folder structure and pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {
-	ConflictIssuesCards,
-	OngoingConflictCard,
-	OpenConflictsCard,
-} from "./components/CardComponents/ConflictIssuesCards";
-import { ContactUsCard } from "./components/CardComponents/ContactAndInfoCards";
-import TransactionHistory from "./components/TableComponents/TransactionHistory";
-import About from "./pages/About";
-import Neutral_dashboard from "./pages/neutral_dashboard";
+import Homepage from "./pages/GENERAL_PAGES/Homepage";
 
 function App() {
 	return (
-		<>
-			<Neutral_dashboard />
-		</>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+			</Routes>
+		</Router>
 	);
 }
 
