ce460691d1111a158a75e792178e759321911955
Korex23 <ololokes@gmail.com>
2022-12-27 17:14:51 +0100

    adjusted the navbar to effect the current changes and started the creation of the Initiate Disputes Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { GuestNavbar } from "./components/NavbarComponents/TopNavbars";
-import { ComplaintForm } from "./components/FormComponents.js/IssuesComplaintForms";
-import { BuyerEscrowInitiationForm } from "./components/FormComponents.js/TransactionForms";
-import InitiateDisputesForm from "./pages/DISPUTE_PAGES/InitiateDisputesForm";
 import Homepage from "./pages/GENERAL_PAGES/Homepage";
 import AboutUs from "./pages/GENERAL_PAGES/AboutUs";
 import ContactUs from "./pages/GENERAL_PAGES/ContactUs";
@@ -13,8 +9,6 @@ import FAQs from "./pages/GENERAL_PAGES/FAQs";
 function App() {
   return (
     <Router>
-      <GuestNavbar />
-      <InitiateDisputesForm />
       <Routes>
         {/* GENERAL PAGES */}
         <Route path="/" element={<Homepage />} />
