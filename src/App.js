b0287d836a12853376bca533df566cb5c07ab2b3
Christian Gabriel <goldenlampstandministry777@gmail.com>
2022-12-30 15:12:52 +0100

    Done with the About Us Page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import PricingPage from "./pages/GENERAL_PAGES/PricingPage";
 import FAQs from "./pages/GENERAL_PAGES/FAQs";
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				{/* GENERAL PAGES */}
-				<Route path="/" element={<Homepage />} />
-				<Route path="aboutus" element={<AboutUs />} />
-				<Route path="contactus" element={<ContactUs />} />
-				<Route path="pricingpage" element={<PricingPage />} />
-				<Route path="faqs" element={<FAQs />} />
-			</Routes>
-		</Router>
-	);
+  return (
+    <AboutUs />
+
+    //     <Router>
+    //
+    //       <Routes>
+    //         {/* GENERAL PAGES */}
+    //         <Route path="/" element={<Homepage />} />
+    //         <Route path="aboutus" element={<AboutUs />} />
+    //         <Route path="contactus" element={<ContactUs />} />
+    //         <Route path="pricingpage" element={<PricingPage />} />
+    //         <Route path="faqs" element={<FAQs />} />
+    //       </Routes>
+    //     </Router>
+  );
 }
 
 export default App;
--- src/components/CardComponents/AboutUsCards.js
+++ src/components/CardComponents/AboutUsCards.js
@@ -1,53 +1,47 @@
 import React from "react";
 import aboutusPix from "../../images/aboutusPix.png";
+// import { aboutusData } from "../../data/aboutusData.json";
 
 // CoreValueCard
-export const CoreValueCard = () => {
-	return (
-		<div class="card" style={{ width: "20rem" }}>
-			<div class="card-body coreValueStyle rounded-1 p-4">
-				<h3 class="card-title text-white">Daily Hard Work</h3>
-				<h6 class="card-subtitle mb-2 text-white">
-					Lorem ipsum dolor sit amet
-				</h6>
-				<p class="card-text text-white">
-					Lorem ipsum dolor sit amet conur adipiscing dolor sit amet dolor sit
-					amet
-				</p>
-			</div>
-		</div>
-	);
+export const CoreValueCard = (props) => {
+  const { title, subtitle, description } = props;
+  return (
+    <div class="card">
+      <div class="card-body CoreValueCard rounded-1 p-4">
+        <h5 class="card-title text-white fw-bold">{title}</h5>
+        <h6 class="card-subtitle mb-2 text-white">{subtitle}</h6>
+        <p class="card-text text-white">{description}</p>
+      </div>
+    </div>
+  );
 };
 
 //TeamsCard;
-export const TeamsCard = () => {
-	return (
-		<div class="card mb-3 teamValueStyle p-3 teamsCard col-lg-6">
-			<div class="row g-0">
-				<div class=" col-lg-5 col-md-4 col-sm-12 ">
-					<img
-						src={aboutusPix}
-						class="img-fluid  rounded-start mx-auto"
-						alt="..."
-					/>
-				</div>
-				<div class="col-lg-7 col-md-8 mt-2 col-sm-12">
-					<div class="card-body d-flex flex-column align-item-center justify-content-center">
-						<h5 class="card-title">Card title</h5>
-						<div className="mt-lg-4">
-							<p class="card-text text-start">
-								This is a wider card with supporting text below as a natural
-								lead-in to additional content. This content is a little bit
-								longer.
-							</p>
-							<div class="card-text text-end me-lg-5 mt-2 ">
-								<h6 className="bold">Christian Gabriel</h6>
-								<p>CEO</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+export const TeamsCard = (props) => {
+  const { name, job_description, description, images } = props;
+  return (
+    <div class="card mb-3 teamValueStyle p-4 shadow-sm border-1">
+      <div class="row g-0">
+        <div class="col-lg-5 col-md-4 col-sm-12">
+          <img
+            src={images}
+            class="img-fluid w-75 rounded-start mx-auto"
+            alt="..."
+          />
+        </div>
+        <div class="col-lg-7 col-md-8 mt-2 col-sm-12">
+          <div class="card-body d-flex flex-column align-item-center justify-content-center">
+            {/* <h5 class="card-title">{name}</h5> */}
+            <div className="mt-lg-4">
+              <p class="card-text text-start">{description}</p>
+              <div class="card-text text-end me-lg-5 mt-2 ">
+                <h6 className="bold">{name}</h6>
+                <p>{job_description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
