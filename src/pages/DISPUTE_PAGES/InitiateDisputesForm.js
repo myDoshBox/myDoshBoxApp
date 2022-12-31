20634ff0ae212bfd30ad9dfe71c25a79b1b75227
Korex23 <ololokes@gmail.com>
2022-12-31 23:24:14 +0100

    switched the navbar from react bootstrap to regular bootstrap, but theres a little issue to be fixed

--- src/pages/DISPUTE_PAGES/InitiateDisputesForm.js
+++ src/pages/DISPUTE_PAGES/InitiateDisputesForm.js
@@ -1,13 +1,137 @@
-import { ComplaintForm } from "../../components/FormComponents.js/IssuesComplaintForms";
+// import { ComplaintForm } from "../../components/FormComponents.js/IssuesComplaintForms";
 import { UserSidenav } from "../../components/NavbarComponents/SideNavbar";
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
 
 const InitiateDisputesForm = () => {
   return (
-    <div>
-      {/* <UserSidenav /> */}
-      <ComplaintForm />
+    <div className="row">
+      <div className="col-sm-12 col-lg-3">
+        <UserSidenav />
+      </div>
+      <div className="col-sm-12 col-lg-9">
+        <ComplaintForm />
+      </div>
     </div>
   );
 };
+
+const ComplaintForm = () => {
+  const [PhoneNumber, setPhoneNumber] = useState("");
+
+  const [TransactionID, setTransactionID] = useState("");
+
+  const [ComplaintType, setComplaintType] = useState("");
+
+  const [File, setFile] = useState("");
+
+  const [ProvideDetails, setProvideDetails] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <Container className="col-md-9">
+        <div className="mt-5 text-center">
+          <h4 className="fw-bold">COMPLAINT FORM</h4>
+        </div>
+
+        {/* Form Section Starts */}
+
+        <form className="form mt-5">
+          <div className="mb-4">
+            <input
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              value={PhoneNumber}
+              placeholder="Phone Number"
+              type="tel"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+
+            <span
+              id="nameHelp"
+              className="form-text text-danger fst-italic fw-lighter"
+            >
+              *This field is required
+            </span>
+          </div>
+          <div className="mb-4">
+            <input
+              onChange={(e) => setTransactionID(e.target.value)}
+              value={TransactionID}
+              placeholder="Transaction ID"
+              type="text"
+              className="form-control"
+            />
+
+            <span
+              id="nameHelp"
+              className="form-text text-danger fst-italic fw-lighter"
+            >
+              *This field is required
+            </span>
+          </div>
+
+          <div>
+            <select
+              onChange={(e) => setComplaintType(e.target.value)}
+              value={ComplaintType}
+              className="form-select mb-4"
+              aria-label="Default select example"
+            >
+              <option selected>Failed Transactions</option>
+              <option value="1">Wrong Items</option>
+              <option value="2">Incomplete Items</option>
+              <option value="3">Incomplete Payment</option>
+              <option value="4">Other</option>
+            </select>
+
+            <span
+              id="nameHelp"
+              className="form-text text-danger fst-italic fw-lighter"
+            >
+              *This field is required
+            </span>
+          </div>
+
+          <div className="input-group mb-3">
+            <input
+              onChange={(e) => setFile(e.target.value)}
+              value={File}
+              type="file"
+              className="form-control"
+              id="inputGroupFile02"
+              placeholder="Attach Image(s)"
+            />
+          </div>
+
+          <div className="mb-3 mt-4">
+            <textarea
+              onChange={(e) => setProvideDetails(e.target.value)}
+              value={ProvideDetails}
+              placeholder="Reasons for contesting this complaint"
+              className="form-control "
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
+
+          <div className="d-grid gap-2 col-2 mx-auto mt-4 ">
+            <button
+              className="btn btn-success"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </Container>
+    </>
+  );
+};
+// ComplaintForm Ends
 
 export default InitiateDisputesForm;
--- src/pages/GENERAL_PAGES/PricingPage.js
+++ src/pages/GENERAL_PAGES/PricingPage.js
@@ -1,7 +1,54 @@
 import React from "react";
 
 const PricingPage = () => {
-	return <div>PricingPage</div>;
+  return (
+    <div className="mx-auto">
+      <PricingTable />
+    </div>
+  );
+};
+
+const PricingTable = () => {
+  return (
+    <div className="mx-auto">
+      <div>
+        <h4>Low Fees</h4>
+        <p className="text-wrap">
+          The low cost of our secure auto escrow service is either paid by the
+          Buyer, the Seller, or shared between them.
+        </p>
+      </div>
+      {/* table Section starts */}
+      <section>
+        <table className=" table table-bordered border-white  w-75 ">
+          <thead className="">
+            <tr className="BgColor ">
+              <th className="text-white p-4 text-center">Transaction Amount</th>
+              <th className="text-white col-7 p-4 text-center">Doshbox Fess</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="p-3 text-center">N1000-N10,000</td>
+              <td className="p-3 text-center">N1000-N10,000</td>
+            </tr>
+            <tr className="TableRowColor">
+              <td className="p-3 text-center">N1000-N10,000</td>
+              <td className="p-3 text-center">N1000-N10,000</td>
+            </tr>
+            <tr>
+              <td className="p-3 text-center">N1000-N10,000</td>
+              <td className="p-3 text-center">N1000-N10,000</td>
+            </tr>
+            <tr className="TableRowColor">
+              <td className="p-3 text-center">N1000-N10,000</td>
+              <td className="p-3 text-center">N1000-N10,000</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
 };
 
 export default PricingPage;
