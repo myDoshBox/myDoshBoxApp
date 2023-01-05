60da1c157bf0623fa8f52adc04dbfd3848ecc09b
profgreatwonder <muoghaluchidinma@gmail.com>
2023-01-05 13:46:27 +0100

    route fixed. Now working with nested routes

--- src/App.js
+++ src/App.js
@@ -5,21 +5,38 @@ import {
 	Route,
 	Navigate,
 } from "react-router-dom";
-import GeneralPagesRoutes from "./pages/ROUTES/GeneralPagesRoutes";
-import UsersDashboardRoutes from "./pages/ROUTES/UsersDashboardRoutes";
 import {
 	NeutralsSidenav,
 	UserSidenav,
 } from "./components/NavbarComponents/SideNavbar";
+import { GuestNavbar } from "./components/NavbarComponents/TopNavbars";
+import Homepage from "./pages/GENERAL_PAGES/Homepage";
+import AboutUs from "./pages/GENERAL_PAGES/AboutUs";
+import ContactUs from "./pages/GENERAL_PAGES/ContactUs";
+import PricingPage from "./pages/GENERAL_PAGES/PricingPage";
+import FAQs from "./pages/GENERAL_PAGES/FAQs";
 import Error404 from "./pages/GENERAL_PAGES/Error404";
+// import GeneralPagesRoutes from "./pages/ROUTES/GeneralPagesRoutes";
+// import UsersDashboardRoutes from "./pages/ROUTES/UsersDashboardRoutes";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
 				{/* GENERAL PAGE ROUTE CAN BE FOUND AT GeneralPagesRoutes */}
-				<Route path="/*" element={<GeneralPagesRoutes />} />
-				<Route path="*" element={<Navigate to={<Error404 />} replace />} />
+				<Route element={<GuestNavbar />}>
+					<Route path="/" element={<Homepage />} />
+					<Route path="aboutus" element={<AboutUs />} />
+					<Route path="contactus" element={<ContactUs />} />
+					<Route path="pricingpage" element={<PricingPage />} />
+					<Route path="faqs" element={<FAQs />} />
+				</Route>
+
+				<Route path="dashboard" element={<NeutralsSidenav />}>
+					<Route path="aboutus" element={<AboutUs />} />
+				</Route>
+				{/* <Route path="*" element={<Navigate to={<Error404 />} />} /> */}
+				<Route path="*" element={<Error404 />} />
 			</Routes>
 		</Router>
 	);
