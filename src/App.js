2f9ef1862f257795b63ac43d5443d6a144bbb827
Korex23 <ololokes@gmail.com>
2023-01-06 12:29:21 +0100

    cleared out app.js

--- src/App.js
+++ src/App.js
@@ -16,30 +16,28 @@ import ContactUs from "./pages/GENERAL_PAGES/ContactUs";
 import PricingPage from "./pages/GENERAL_PAGES/PricingPage";
 import FAQs from "./pages/GENERAL_PAGES/FAQs";
 import Error404 from "./pages/GENERAL_PAGES/Error404";
-import ContestComplaintForm from "./pages/DISPUTE_PAGES/ContestComplaintForm";
 // import GeneralPagesRoutes from "./pages/ROUTES/GeneralPagesRoutes";
 // import UsersDashboardRoutes from "./pages/ROUTES/UsersDashboardRoutes";
 
 function App() {
   return (
     <Router>
-      <ContestComplaintForm />
-      {/* <Routes> */}
-      {/* GENERAL PAGE ROUTE CAN BE FOUND AT GeneralPagesRoutes */}
-      {/* <Route element={<GuestNavbar />}>
+      <Routes>
+        {/* GENERAL PAGE ROUTE CAN BE FOUND AT GeneralPagesRoutes */}
+        <Route element={<GuestNavbar />}>
           <Route path="/" element={<Homepage />} />
           <Route path="aboutus" element={<AboutUs />} />
           <Route path="contactus" element={<ContactUs />} />
           <Route path="pricingpage" element={<PricingPage />} />
           <Route path="faqs" element={<FAQs />} />
-        </Route> */}
+        </Route>
 
-      {/* <Route path="dashboard" element={<NeutralsSidenav />}>
+        <Route path="dashboard" element={<NeutralsSidenav />}>
           <Route path="aboutus" element={<AboutUs />} />
-        </Route> */}
-      {/* <Route path="*" element={<Navigate to={<Error404 />} />} /> */}
-      {/* <Route path="*" element={<Error404 />} />
-      </Routes> */}
+        </Route>
+        {/* <Route path="*" element={<Navigate to={<Error404 />} />} /> */}
+        <Route path="*" element={<Error404 />} />
+      </Routes>
     </Router>
   );
 }
