c8cad2988450ee4b9d308d8176acd27f09125d52
Korex23 <ololokes@gmail.com>
2023-01-09 15:12:41 +0100

    reduced the size of the user side nav and completed the user settings page

--- src/App.js
+++ src/App.js
@@ -16,27 +16,39 @@ import ContactUs from "./pages/GENERAL_PAGES/ContactUs";
 import PricingPage from "./pages/GENERAL_PAGES/PricingPage";
 import FAQs from "./pages/GENERAL_PAGES/FAQs";
 import Error404 from "./pages/GENERAL_PAGES/Error404";
+import { MiniProfileCard } from "./components/CardComponents/UsersCards";
+import { FeedbackIcon } from "./components/IconComponent/UserdashboardIcons";
+import {
+  BigLogoutIcon,
+  ChangeBankIcon,
+  ContactUsIcon,
+  FaqIcon,
+  UpdateProfileIcon,
+} from "./components/IconComponent/UserdashboardIcons";
+import UserSettingsPage from "./pages/USER_DASHBOARD_PAGES/UserSettingsPage";
+
 // import GeneralPagesRoutes from "./pages/ROUTES/GeneralPagesRoutes";
 // import UsersDashboardRoutes from "./pages/ROUTES/UsersDashboardRoutes";
 
 function App() {
   return (
     <Router>
+      <UserSettingsPage />
       <Routes>
         {/* GENERAL PAGE ROUTE CAN BE FOUND AT GeneralPagesRoutes */}
-        <Route element={<GuestNavbar />}>
+        {/* <Route element={<GuestNavbar />}>
           <Route path="/" element={<Homepage />} />
           <Route path="aboutus" element={<AboutUs />} />
           <Route path="contactus" element={<ContactUs />} />
           <Route path="pricingpage" element={<PricingPage />} />
           <Route path="faqs" element={<FAQs />} />
-        </Route>
+        </Route> */}
 
-        <Route path="dashboard" element={<NeutralsSidenav />}>
+        {/* <Route path="dashboard" element={<NeutralsSidenav />}>
           <Route path="aboutus" element={<AboutUs />} />
-        </Route>
+        </Route> */}
         {/* <Route path="*" element={<Navigate to={<Error404 />} />} /> */}
-        <Route path="*" element={<Error404 />} />
+        {/* <Route path="*" element={<Error404 />} /> */}
       </Routes>
     </Router>
   );
