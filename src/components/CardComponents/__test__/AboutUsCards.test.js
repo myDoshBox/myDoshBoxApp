ed9af492c40ebeed2f5fd852453baafab4e522a8
Oluwasheyi <martinsakin7@gmail.com>
2023-03-22 05:28:12 -0700

    ok

--- src/components/CardComponents/__test__/AboutUsCards.test.js
+++ src/components/CardComponents/__test__/AboutUsCards.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import AboutUsCard from "../AboutUsCards";
 
-it("should get text render into title prop", () => {
-  render(<AboutUsCard title="My header" />);
-  const headingElement = screen.getByText(/My header/i);
-  expect(headingElement).toBeInTheDocument();
+describe("AboutUs", () => {
+  it("should get text render into title prop", () => {
+    render(<AboutUsCard title="My header" />);
+    const headingElement = screen.getByText(/My header/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 });
