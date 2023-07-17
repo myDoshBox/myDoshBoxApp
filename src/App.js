7261f5fa1c8782be1fce641e3a7bec882e540bb6
profgreatwonder <muoghaluchidinma@gmail.com>
2023-07-17 15:04:42 +0100

    corrected the folder structure and created a new branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
+	BrowserRouter as Router,
+	Routes,
+	Route,
 
-  // Navigate,
+	// Navigate,
 } from "react-router-dom";
 import { GuestNavbar } from "./components/NavbarComponents/TopNavbars";
 import Homepage from "./pages/GENERAL_PAGES/Homepage";
@@ -20,9 +20,9 @@ import NotifictionPage from "./pages/DASHBOARDS/USER_DASHBOARD/NotifictionPage";
 // import FAQs from "./pages/GENERAL_PAGES/FAQs";
 import UserDisputeHistory from "./pages/DASHBOARDS/USER_DASHBOARD/UserDisputesHistory";
 import UserSettingsPage, {
-  ReportIssuesformPage,
-  UpdateBankDetailsPage,
-  UpdateProfilePage,
+	ReportIssuesformPage,
+	UpdateBankDetailsPage,
+	UpdateProfilePage,
 } from "./pages/DASHBOARDS/USER_DASHBOARD/UserSettingsPage";
 import UserTransactionHistory from "./pages/DASHBOARDS/USER_DASHBOARD/UserTransactionHistory";
 import NeutralsSideNav from "./components/NavbarComponents/NeutralsSideNav";
@@ -34,9 +34,9 @@ import CustomerCareSideNav from "./components/NavbarComponents/CustomerCareSideN
 import { NeutralSetting } from "./pages/NEUTRALS_PAGES/NeutralSetting";
 import NeutralDashboard from "./pages/NEUTRALS_PAGES/neutral_dashboard";
 import {
-  ClosedConflicts,
-  OngoingConflicts,
-  OpenConflicts,
+	ClosedConflicts,
+	OngoingConflicts,
+	OpenConflicts,
 } from "./pages/DISPUTE_PAGES/Conflicts";
 import SignInPage from "./pages/AUTHENTICATION_PAGES/SignIn";
 import SignUpPage from "./pages/AUTHENTICATION_PAGES/SignUp";
@@ -59,75 +59,75 @@ import AdminSideNav from "./components/NavbarComponents/AdminSideNav";
 import UserProfile from "./pages/DASHBOARDS/USER_DASHBOARD/UserProfile";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* GENERAL PAGE ROUTE CAN BE FOUND AT GeneralPagesRoutes */}
-        <Route element={<GuestNavbar />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/pricingpage" element={<PricingPage />} />
-          <Route path="/faqs" element={<FAQs />} />
-        </Route>
-        <Route path="userdashboard" element={<UsersSideNav />}>
-          <Route index element={<UserDashboardPage />} />
-          <Route path="settings" element={<UserSettingsPage />} />
-          <Route path="transaction" element={<UserTransactionHistory />} />
-          <Route path="dispute" element={<UserDisputeHistory />} />
-          <Route path="notification" element={<NotifictionPage />} />
-          <Route path="updateprofile" element={<UpdateProfilePage />} />
-          <Route path="updatebank" element={<UpdateBankDetailsPage />} />
-          <Route path="reportissues" element={<ReportIssuesformPage />} />
-          <Route path="initiate-escrow" element={<InitiateEscrow />} />
-          <Route path="userspage" element={<UsersPage />} />
+	return (
+		<Router>
+			<Routes>
+				{/* GENERAL PAGE ROUTE CAN BE FOUND AT GeneralPagesRoutes */}
+				<Route element={<GuestNavbar />}>
+					<Route path="/" element={<Homepage />} />
+					<Route path="/aboutus" element={<AboutUs />} />
+					<Route path="/contactus" element={<ContactUs />} />
+					<Route path="/pricingpage" element={<PricingPage />} />
+					<Route path="/faqs" element={<FAQs />} />
+				</Route>
+				<Route path="userdashboard" element={<UsersSideNav />}>
+					<Route index element={<UserDashboardPage />} />
+					<Route path="settings" element={<UserSettingsPage />} />
+					<Route path="transaction" element={<UserTransactionHistory />} />
+					<Route path="dispute" element={<UserDisputeHistory />} />
+					<Route path="notification" element={<NotifictionPage />} />
+					<Route path="updateprofile" element={<UpdateProfilePage />} />
+					<Route path="updatebank" element={<UpdateBankDetailsPage />} />
+					<Route path="reportissues" element={<ReportIssuesformPage />} />
+					<Route path="initiate-escrow" element={<InitiateEscrow />} />
+					<Route path="userspage" element={<UsersPage />} />
 
-          <Route
-            path="transactionsummary"
-            element={<TransactionSummaryPage />}
-          />
-          <Route path="agreement" element={<EscrowAgreement />} />
-          <Route path="initiate-dispute" element={<InitiateDisputesForm />} />
-          <Route path="ticket" element={<GeneratedTicket />} />
-        </Route>
-        <Route path="neutraldashboard" element={<NeutralsSideNav />}>
-          <Route index element={<NeutralDashboard />} />
-          <Route path="open-conflicts" element={<OpenConflicts />} />
-          <Route path="closed-conflicts" element={<ClosedConflicts />} />
-          <Route path="transactions" element={<CustomerCareTransaction />} />
-          <Route path="ongoing-conflicts" element={<OngoingConflicts />} />
-          <Route path="neutralsetting" element={<NeutralSetting />} />
-          <Route path="notification" element={<NotifictionPage />} />
-        </Route>
-        <Route path="customer-care" element={<CustomerCareSideNav />}>
-          <Route index element={<CustomerCareDashboardPage />} />
-          <Route path="tickets-history" element={<TicketHistoryPage />} />
-          <Route path="notification" element={<NotifictionPage />} />
-          <Route path="settings" element={<NeutralSetting />} />
-          <Route path="transactions" element={<CustomerCareTransaction />} />
-          <Route path="users" element={<UsersPage />} />
-        </Route>
+					<Route
+						path="transactionsummary"
+						element={<TransactionSummaryPage />}
+					/>
+					<Route path="agreement" element={<EscrowAgreement />} />
+					<Route path="initiate-dispute" element={<InitiateDisputesForm />} />
+					<Route path="ticket" element={<GeneratedTicket />} />
+				</Route>
+				<Route path="neutraldashboard" element={<NeutralsSideNav />}>
+					<Route index element={<NeutralDashboard />} />
+					<Route path="open-conflicts" element={<OpenConflicts />} />
+					<Route path="closed-conflicts" element={<ClosedConflicts />} />
+					<Route path="transactions" element={<CustomerCareTransaction />} />
+					<Route path="ongoing-conflicts" element={<OngoingConflicts />} />
+					<Route path="neutralsetting" element={<NeutralSetting />} />
+					<Route path="notification" element={<NotifictionPage />} />
+				</Route>
+				<Route path="customer-care" element={<CustomerCareSideNav />}>
+					<Route index element={<CustomerCareDashboardPage />} />
+					<Route path="tickets-history" element={<TicketHistoryPage />} />
+					<Route path="notification" element={<NotifictionPage />} />
+					<Route path="settings" element={<NeutralSetting />} />
+					<Route path="transactions" element={<CustomerCareTransaction />} />
+					<Route path="users" element={<UsersPage />} />
+				</Route>
 
-        {/* Admin Routes */}
-        <Route path="admin" element={<AdminSideNav />}>
-          <Route index element={<AdminDashboard />} />
-          <Route path="tickets-history" element={<TicketHistoryPage />} />
-          <Route path="profile" element={<UserProfile />} />
-          <Route path="AdminAnalytics" element={<AdminAnalytics />} />
-          <Route path="notification" element={<NotifictionPage />} />
-          <Route path="settings" element={<NeutralSetting />} />
-          <Route path="transactions" element={<CustomerCareTransaction />} />
-          <Route path="users" element={<UsersPage />} />
-        </Route>
-        {/* <Route path="*" element={<Navigate to={<Error404 />} />} /> */}
-        <Route path="signin" element={<SignInPage />} />
-        <Route path="signup" element={<SignUpPage />} />
-        {/* <Route path="*" element={<Navigate to={<Error404 />} />} /> */}
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
+				{/* Admin Routes */}
+				<Route path="admin" element={<AdminSideNav />}>
+					<Route index element={<AdminDashboard />} />
+					<Route path="tickets-history" element={<TicketHistoryPage />} />
+					<Route path="profile" element={<UserProfile />} />
+					<Route path="AdminAnalytics" element={<AdminAnalytics />} />
+					<Route path="notification" element={<NotifictionPage />} />
+					<Route path="settings" element={<NeutralSetting />} />
+					<Route path="transactions" element={<CustomerCareTransaction />} />
+					<Route path="users" element={<UsersPage />} />
+				</Route>
+				{/* <Route path="*" element={<Navigate to={<Error404 />} />} /> */}
+				<Route path="signin" element={<SignInPage />} />
+				<Route path="signup" element={<SignUpPage />} />
+				{/* <Route path="*" element={<Navigate to={<Error404 />} />} /> */}
+				<Route path="*" element={<Error404 />} />
+			</Routes>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
--- src/components/TableComponents/TransactionTable.js
+++ src/components/TableComponents/TransactionTable.js
@@ -11,113 +11,112 @@ import { Link } from "react-router-dom";
 import { ViewMoreButton } from "../ButtonsComponent/NavigationAndViewButtons";
 
 export const NotificationCard = (props) => {
-  const { image, title, description, link } = props;
-  return (
-    <div className="d-flex justify-content-around align-items-center border-bottom p-2">
-      <img src={image} alt="SampleUserImg" className="" />
-      <h6 className="d-none d-lg-block mx-3">{title}</h6>
-      <div className="d-inline">
-        <p className="w-100 ps-3">{description}</p>
-      </div>
-      <button className="btn text-primary border-0 d-none d-md-block">
-        {link}
-      </button>
-    </div>
-  );
+	const { image, title, description, link } = props;
+	return (
+		<div className="d-flex justify-content-around align-items-center border-bottom p-2">
+			<img src={image} alt="SampleUserImg" className="" />
+			<h6 className="d-none d-lg-block mx-3">{title}</h6>
+			<div className="d-inline">
+				<p className="w-100 ps-3">{description}</p>
+			</div>
+			<button className="btn text-primary border-0 d-none d-md-block">
+				{link}
+			</button>
+		</div>
+	);
 };
 
 //RecentTransactionTable
 
 export const MiniRecentTransactionTable = () => {
-  return (
-    <div className="border shadow p-3" style={{ width: "100%" }}>
-      {/* <div className="col-lg-9 border shadow" style={{ width: "100%" }}> */}
-      <div>
-        {/* <div className="px-4 mb-3">
+	return (
+		<div className="border shadow p-3" style={{ width: "100%" }}>
+			{/* <div className="col-lg-9 border shadow" style={{ width: "100%" }}> */}
+			<div>
+				{/* <div className="px-4 mb-3">
           <Link to={"../transaction"}>
             <GeneralBtnStyle1 text="New Transaction" />
           </Link>
         </div> */}
 
-        <table className="table transaction-table">
-          <thead className="text-center">
-            <tr>
-              <th>Product Name</th>
-              <th>Amount</th>
-              <th>Date</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {TransactionData.mini_transaction.map((mini) => {
-              return <RecentTransactionTableData {...mini} key={mini.id} />;
-            })}
-          </tbody>
-        </table>
-        <div className="p-4 text-md-end d-none d-md-block">
-          <Link to={"./transaction"}>
-            <ViewMoreButton />
-          </Link>
-        </div>
-        <div className="text-center d-lg-none .d-xl-block">
-          <Link to={"./transaction"}>
-            <ViewMoreButton />
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+				<table className="table transaction-table">
+					<thead className="text-center">
+						<tr>
+							<th>Product Name</th>
+							<th>Amount</th>
+							<th>Date</th>
+							<th>Status</th>
+						</tr>
+					</thead>
+					<tbody>
+						{TransactionData.mini_transaction.map((mini) => {
+							return <RecentTransactionTableData {...mini} key={mini.id} />;
+						})}
+					</tbody>
+				</table>
+				<div className="p-4 text-md-end d-none d-md-block">
+					<Link to={"./transaction"}>
+						<ViewMoreButton />
+					</Link>
+				</div>
+				<div className="text-center d-lg-none .d-xl-block">
+					<Link to={"./transaction"}>
+						<ViewMoreButton />
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 // AdminRecentTransactionsCard
 export const AdminRecentTransactionsCard = memo(({ style }) => {
-  console.count("AdminRecentTransactionsCard: ");
-  return (
-    <div className={`${style} mb-3 mb-md-0`}>
-      <div className={`card p-3 shadow rounded border-0 h-100`}>
-        <h6 className="pb-3 m-0">Recent Transactions</h6>
-        <table class="table m-0 h-100">
-          <thead>
-            <tr>
-              <th scope="col" className="opacity-50 text-center">
-                Transaction Id
-              </th>
-              <th scope="col" className="opacity-50 text-center">
-                Status
-              </th>
-              <th scope="col" className="opacity-50 text-center">
-                ...
-              </th>
-            </tr>
-          </thead>
-          <tbody className="h-100">
-            {TransactionData.mini_transaction.map((mini) => {
-              return (
-                <>
-                  <tr key={mini.id} className="text-center border-bottom">
-                    <td className="p-md-3 text-small">{mini.id}</td>
-                    {/* <td className="p-md-3 text-small">{mini.status_name}</td> */}
-                    <td className="p-md-3 d-flex justify-content-center align-items-center border-0">
-                      <Notifications
-                        text={mini.status_name}
-                        styles={`text-small ${mini.status_style} border-0`}
-                      />
-                    </td>
-                    <td className="p-md-3 text-small">View</td>
-                  </tr>
-                </>
-              );
-            })}
-            <Link
-              to="../transaction"
-              className="d-flex justify-content-center pt-3 text-decoration-none w-100"
-              style={{ whiteSpace: "nowrap" }}
-            >
-              <ViewMoreButton />
-            </Link>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+	console.count("AdminRecentTransactionsCard: ");
+	return (
+		<div className={`${style} mb-3 mb-md-0`}>
+			<div className={`card p-3 shadow rounded border-0 h-100`}>
+				<h6 className="pb-3 m-0">Recent Transactions</h6>
+				<table class="table m-0 h-100">
+					<thead>
+						<tr>
+							<th scope="col" className="opacity-50 text-center">
+								Transaction Id
+							</th>
+							<th scope="col" className="opacity-50 text-center">
+								Status
+							</th>
+							<th scope="col" className="opacity-50 text-center">
+								...
+							</th>
+						</tr>
+					</thead>
+					<tbody className="h-100">
+						{TransactionData.mini_transaction.map((mini) => {
+							return (
+								<>
+									<tr key={mini.id} className="text-center border-bottom">
+										<td className="p-md-3 text-small">{mini.id}</td>
+										{/* <td className="p-md-3 text-small">{mini.status_name}</td> */}
+										<td className="p-md-3 d-flex justify-content-center align-items-center border-0">
+											<Notifications
+												text={mini.status_name}
+												styles={`text-small ${mini.status_style} border-0`}
+											/>
+										</td>
+										<td className="p-md-3 text-small">View</td>
+									</tr>
+								</>
+							);
+						})}
+						<Link
+							to="../transaction"
+							className="d-flex justify-content-center pt-3 text-decoration-none w-100"
+							style={{ whiteSpace: "nowrap" }}>
+							<ViewMoreButton />
+						</Link>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 });
